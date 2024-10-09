868446461687466312b955a6686152b5086778b8
Leonard Laput <online.lorno@gmail.com>
2024-10-09 21:05:28 +0800

    ✨ Header component updates

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,20 +1,38 @@
 "use client";
 
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Container, styled, Toolbar, Typography } from "@mui/material";
+import { useAppSelector } from "@/redux/hooks";
 import ElevationScroll from "./ElevationScroll";
+import HeaderUserMenu from "./HeaderUserMenu";
 
 const Header = () => {
+  const email = useAppSelector((state) => state.auth.email);
+
   return (
     <ElevationScroll>
       <AppBar>
-        <Toolbar>
-          <Typography variant="h6" component="div" className="font-mono">
-            Task Management
-          </Typography>
-        </Toolbar>
+        <Container>
+          <Toolbar
+            disableGutters
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <LogoText variant="h6">scaling</LogoText>
+            {email && <HeaderUserMenu />}
+          </Toolbar>
+        </Container>
       </AppBar>
     </ElevationScroll>
   );
 };
+
+const LogoText = styled(Typography)({
+  letterSpacing: ".3rem",
+  fontFamily: "var(--font-geist-mono)",
+  fontWeight: 700,
+});
 
 export default Header;
---
+++ frontend/src/components/HeaderUserMenu.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import {
+  Box,
+  Tooltip,
+  IconButton,
+  Avatar,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+} from "@mui/material";
+import { Logout } from "@mui/icons-material";
+import { useAppSelector } from "@/redux/hooks";
+
+const HeaderUserMenu = () => {
+  const email = useAppSelector((state) => state.auth.email);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleLogout = () => {
+    // logout logic
+    handleCloseUserMenu();
+  };
+
+  return (
+    <Box>
+      <Tooltip title="Account settings">
+        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+          <Avatar alt={email ?? "user"} />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        sx={{ mt: "45px" }}
+        id="menu-appbar"
+        anchorEl={anchorElUser}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={Boolean(anchorElUser)}
+        onClose={handleCloseUserMenu}
+      >
+        <MenuItem onClick={handleLogout}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  );
+};
+
+export default HeaderUserMenu;

cb4bac7e5157219bd03dc6440490e787cb8672ad
Leonard Laput <online.lorno@gmail.com>
2024-10-09 21:04:24 +0800

    ♻️ Update auth slice to add email

--- frontend/src/redux/slices/auth-slice.ts
+++ frontend/src/redux/slices/auth-slice.ts
@@ -2,21 +2,25 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface AuthState {
   token: string | null;
+  email: string | null;
 }
 
 const initialState: AuthState = {
   token: null,
+  email: null,
 };
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    login: (state, action: PayloadAction<{ token: string }>) => {
+    login: (state, action: PayloadAction<{ token: string; email: string }>) => {
       state.token = action.payload.token;
+      state.email = action.payload.email;
     },
     logout: (state) => {
       state.token = null;
+      state.email = null;
     },
   },
 });
